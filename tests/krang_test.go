7e6e4f8fd5ed9a504c66df800b3d7a291ad3f26a
Alexey Pokhozhaev <poporul@gmail.com>
2017-09-06 16:42:43 +0300

    Add test for damaged input :white_check_mark:

--- tests/krang_test.go
+++ tests/krang_test.go
@@ -12,11 +12,18 @@ const (
 	BAD = "[31m✘[0m"
 )
 
-var data = map[string]int{
-	"2 + 3 * 4 - 5 + 7 * 6 / 3 - 2 * 3 ^ 2 + ( 5 - 2 ) * 2": 11,
-	"3 + 4 * 2 / ( 5 - 1 ) ^ 2 ^ 3 + 2":                     5,
-	"   4 -(  1 +3) + 81 /9 ":                               9,
-}
+var (
+	data = map[string]int{
+		"2 + 3 * 4 - 5 + 7 * 6 / 3 - 2 * 3 ^ 2 + ( 5 - 2 ) * 2": 11,
+		"3 + 4 * 2 / ( 5 - 1 ) ^ 2 ^ 3 + 2":                     5,
+		"   4 -(  1 +3) + 81 /9 ":                               9,
+	}
+
+	damaged = []string{
+		"1 + a",
+		"10 .* ( 9 )",
+	}
+)
 
 func buildLog(status, source string, expected, actual int) string {
 	return fmt.Sprintf(
@@ -32,6 +39,18 @@ func TestEval(t *testing.T) {
 			t.Error(buildLog(BAD, source, expected, actual))
 		} else {
 			t.Log(buildLog(OK, source, expected, actual))
+		}
+	}
+}
+
+func TestInvalidInput(t *testing.T) {
+	for _, input := range damaged {
+		_, err := krang.Tokenize("1 + a")
+
+		if err != krang.InvalidInputError {
+			t.Error(BAD, input)
+		} else {
+			t.Log(OK, input)
 		}
 	}
 }
